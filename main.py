f3d19fb2a4623046398e3e1f570d7035e7b73dd2
IvanPro91 <ivan-don4encko@ya.ru>
2025-02-08 21:29:28 +0300

    Change return in processing func filter_by_state, chk linters

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from src.processing import filter_by_state, sort_by_date
-from src.widget import get_date, mask_account_card
 
-default_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+default_data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 if __name__ == "__main__":
-    test_0 = mask_account_card("Visa Plantum 0123456789012345")
-    test_1 = mask_account_card("Visa 2423423789542347")
-    test_2 = mask_account_card("Счет 01234567896587412301")
-    date = get_date("2025-01-27T13:37:18.671407")
     filter_state = filter_by_state(default_data, state="CANCELED")
     sorted_date = sort_by_date(default_data)
-
-    print(test_0)
-    print(test_1)
-    print(test_2)
-    print(date)
 
     print(filter_state)
     print(sorted_date)
--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,4 @@
-
-def filter_by_state(list_data:list[dict], state:str="EXECUTED") -> list[dict]:
+def filter_by_state(list_data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению.
@@ -7,13 +6,14 @@ def filter_by_state(list_data:list[dict], state:str="EXECUTED") -> list[dict]:
     :param state: Значение для ключа
     :return: Список словарей отсортированные по ключу
     """
-    return [l for l in list_data if l['state'] == state]
+    return [dict_data_state for dict_data_state in list_data if dict_data_state["state"] == state]
 
-def sort_by_date(list_data:list[dict], sort_reverse:bool=True) -> list[dict]:
+
+def sort_by_date(list_data: list[dict], sort_reverse: bool = True) -> list[dict]:
     """
     Функция возвращает новый список, отсортированный по дате (date).
     :param list_data: Список словарей
     :param sort_reverse: Порядок сортировки
     :return: Возврат отсортированного листа со списком
     """
-    return sorted(list_data, key=lambda x: x['date'], reverse=sort_reverse)
+    return sorted(list_data, key=lambda x: x["date"], reverse=sort_reverse)
