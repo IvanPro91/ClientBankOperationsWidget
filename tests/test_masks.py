c99d5cf74f47acbe823ee934b319ea950e3d7c49
IvanPro91 <ivan-don4encko@ya.ru>
2025-02-10 08:05:37 +0300

    change tests for masks. Edit function get_mask_account and update pytest-cov

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -10,5 +10,10 @@ from src.masks import get_mask_account, account_card
 def test_get_mask_card_number(number, result):
     assert account_card(number) == result
 
-def test_get_mask_account(account_number):
-    assert get_mask_account(account_number) == "**4511"
+@pytest.mark.parametrize("number, result", [
+    ("9866548755412145", "**2145"),
+    ("1154122", "**4122"),
+    ("", ""),
+])
+def test_get_mask_account(number, result):
+    assert get_mask_account(number) == result
