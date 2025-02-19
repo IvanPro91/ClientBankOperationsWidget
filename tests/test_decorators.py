c0fde0a5e7cb83ac64e773d0310645fa07948bce
IvanPro91 <ivan-don4encko@ya.ru>
2025-02-19 16:19:45 +0300

    Add decoration and test_decorations

---
+++ tests/test_decorators.py
@@ -0,0 +1,39 @@
+import pytest
+from _pytest.capture import CaptureFixture
+
+from src.decorators import log
+
+
+def test_decorators() -> None:
+    """
+    Функция тестирования декоратора log с параметром
+    :return: None
+    """
+
+    @log(filename="error_log.txt")
+    def num_div(x: int, y: int) -> float:
+        return x / y
+
+    decorator_1 = num_div(6, 2)
+    assert decorator_1 == 3
+
+    with pytest.raises(ZeroDivisionError):
+        num_div(6, 0)
+
+
+def test_capsys_decorators(capsys: CaptureFixture[str]) -> None:
+    """
+    Функция тестирования декоратора log без параметра
+    :return: None
+    """
+
+    @log()
+    def num_div(x: int, y: int) -> float:
+        return x / y
+
+    num_div(6, 2)
+    read_out = capsys.readouterr()
+    assert read_out.out == "num_div ok\n"
+
+    with pytest.raises(ZeroDivisionError):
+        num_div(6, 0)
