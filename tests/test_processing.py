b994367c11ecb530f8d23b9f0240cb7920cad829
IvanPro91 <ivan-don4encko@ya.ru>
2025-02-19 20:04:14 +0300

    code review

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,52 +5,32 @@ from src.processing import filter_by_state, sort_by_date
 
 @pytest.mark.parametrize(
     "state, result",
-    [
-        (
-            "EXECUTED",
-            [
-                {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
-                {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
-            ],
-        ),
-        (
-            "CANCELED",
-            [
-                {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
-                {"date": "2018-10-14T08:21:33.419441", "id": 615064591, "state": "CANCELED"},
-            ],
-        ),
-        ("", []),
-    ],
+    [("EXECUTED", [41428829, 939719570]), ("CANCELED", [594226727, 615064591]), ("", [])],
 )
-def test_filter_by_state(
-    filter_data_and_sorted: list[dict[str, str | int]], state: str, result: list[dict[str, str | int]]
-) -> None:
+def test_filter_by_state(static_data: list[dict], state: str, result: list[dict]) -> None:
     """
     Функция фильтра значений по ключу state
-    :param filter_data_and_sorted: Тестовые данные
+    :param static_data: Тестовые данные
     :param state: Аргумент фильтра значения
     :param result: Результативные данные с параметризации
     :return: None
     """
-    assert filter_by_state(filter_data_and_sorted, state=state) == result
+    get_func_data = filter_by_state(static_data, state=state)
+    list_ids = [id_data_dict["id"] for id_data_dict in get_func_data]
+    assert list_ids == result
 
 
-def test_sort_by_date(filter_data_and_sorted: list[dict[str, str | int]]) -> None:
+def test_sort_by_date(static_data: list[dict]) -> None:
     """
     Функция сортировки данных по дате
-    :param filter_data_and_sorted: Тестовые данные
+    :param static_data: Тестовые данные
     :return: None
     """
-    assert sort_by_date(filter_data_and_sorted) == [
-        {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
-        {"date": "2018-10-14T08:21:33.419441", "id": 615064591, "state": "CANCELED"},
-        {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
-        {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
-    ]
-    assert sort_by_date(filter_data_and_sorted, sort_reverse=False) == [
-        {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
-        {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
-        {"date": "2018-10-14T08:21:33.419441", "id": 615064591, "state": "CANCELED"},
-        {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
-    ]
+    sort_data = sort_by_date(static_data)
+    false_reverse_data = sort_by_date(static_data, sort_reverse=False)
+
+    list_ids = [id_sort_data["id"] for id_sort_data in sort_data]
+    list_false_reverse_ids = [id_sort_data["id"] for id_sort_data in false_reverse_data]
+
+    assert list_ids == [41428829, 615064591, 594226727, 939719570]
+    assert list_false_reverse_ids == [939719570, 594226727, 615064591, 41428829]
