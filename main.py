cb5f0b7cc99ee9a485c70876e65c79277c00910b
IvanPro91 <ivan-don4encko@ya.ru>
2025-03-17 21:41:30 +0300

    Обновление реализации домашней работы

--- main.py
+++ main.py
@@ -9,30 +9,20 @@ from src.widget import get_date, mask_account_card
 
 ROOT_DIR = os.path.dirname(__file__)
 
-DIRECTORY_FILE = {
-    1: f"{ROOT_DIR}/data/operations.json",
-    2: f"{ROOT_DIR}/data/transactions.csv",
-    3: f"{ROOT_DIR}/data/transactions_excel.xlsx",
-}
-
 func_answer = {
-    1: "Получить информацию о транзакциях из JSON-файла",
-    2: "Получить информацию о транзакциях из CSV-файла",
-    3: "Получить информацию о транзакциях из XLSX-файла",
+    1: ("Получить информацию о транзакциях из JSON-файла", f"{ROOT_DIR}/data/operations.json"),
+    2: ("Получить информацию о транзакциях из CSV-файла", f"{ROOT_DIR}/data/transactions.csv"),
+    3: ("Получить информацию о транзакциях из XLSX-файла", f"{ROOT_DIR}/data/transactions_excel.xlsx"),
 }
-
-status = ["EXECUTED", "CANCELED", "PENDING"]
-answer_yes_no = {"Да": True, "Нет": False}
-sorted_min_max = {"по возрастанию": False, "по убыванию": True}
-
 
 def get_user_menu() -> int:
     for answer in func_answer.keys():
-        print(f"{answer}. {func_answer[answer]}")
+        print(f"{answer}. {func_answer[answer][0]}")
     user_menu_input = input().strip()
-    print(f"{func_answer[int(user_menu_input)]}")
+    print(f"{func_answer[int(user_menu_input)][0]}")
     return int(user_menu_input)
 
+status = ["EXECUTED", "CANCELED", "PENDING"]
 
 def get_user_status() -> str:
     while True:
@@ -47,12 +37,16 @@ def get_user_status() -> str:
             print(f'Операции отфильтрованы по статусу "{upper_filter_user}"')
             return upper_filter_user
 
+answer_yes_no = {"Да": True, "Нет": False}
 
-def get_user_input_sorted() -> bool:
+def get_user_answer() -> bool:
     input_data = input().strip()
     if input_data.title() in answer_yes_no.keys():
         return answer_yes_no[input_data.title()]
     return answer_yes_no["Да"]
+
+
+sorted_min_max = {"по возрастанию": False, "по убыванию": True}
 
 
 def get_user_min_max_sorted() -> bool:
@@ -62,51 +56,36 @@ def get_user_min_max_sorted() -> bool:
     return sorted_min_max["по возрастанию"]
 
 
-def get_user_rub_transactions() -> bool:
-    input_data = input().strip()
-    if input_data.title() in answer_yes_no.keys():
-        return answer_yes_no[input_data.title()]
-    return answer_yes_no["Да"]
-
-
-def get_user_filter_word() -> bool:
-    input_data = input().strip()
-    if input_data.title() in answer_yes_no.keys():
-        return answer_yes_no[input_data.title()]
-    return answer_yes_no["Да"]
-
+menu = {
+    1: read_json_file,
+    2: read_finance_csv_operation,
+    3: read_finance_excel_operation
+}
 
 def main() -> None:
     print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
     print("Выберите необходимый пункт меню ниже:")
-    num_menu_input = get_user_menu()
-    if num_menu_input == 1:
-        data_read_file = read_json_file(DIRECTORY_FILE[num_menu_input])
-    elif num_menu_input == 2:
-        data_read_file = read_finance_csv_operation(DIRECTORY_FILE[num_menu_input])
-    elif num_menu_input == 3:
-        data_read_file = read_finance_excel_operation(DIRECTORY_FILE[num_menu_input])
-    else:
-        data_read_file = [{}]
+    num_menu = get_user_menu()
+    data_read_file = menu[num_menu](func_answer[num_menu][1])
 
     print("Введите статус, по которому необходимо выполнить фильтрацию.")
     state = get_user_status()
     current_data = filter_by_state(data_read_file, state=state)
 
     print("Отсортировать операции по дате? Да/Нет")
-    sort_date = get_user_input_sorted()
+    sort_date = get_user_answer()
     if sort_date:
         print("Отсортировать по возрастанию или по убыванию?")
         sorted_min_max_value = get_user_min_max_sorted()
         current_data = sort_by_date(current_data, sort_reverse=sorted_min_max_value)
 
     print("Выводить только рублевые транзакции? Да/Нет")
-    only_rub_transactions = get_user_rub_transactions()
+    only_rub_transactions = get_user_answer()
     currency = "RUB" if only_rub_transactions else "USD"
     current_data = list(filter_by_currency(current_data, currency=currency))
 
     print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
-    filter_word = get_user_filter_word()
+    filter_word = get_user_answer()
     if filter_word:
         find_word = input("Введите искомое слово\n").strip()
         current_data = filter_by_word(current_data, word=find_word)
@@ -114,6 +93,10 @@ def main() -> None:
     print("Распечатываю итоговый список транзакций...")
     print(f"Всего банковских операций в выборке: {len(current_data)}")
 
+    result_main(current_data)
+
+
+def result_main(current_data: list[dict]) -> None:
     for transaction in current_data:
         date = get_date(transaction["date"])
         amount = transaction["amount"] if "amount" in transaction else transaction["operationAmount"]["amount"]
@@ -125,7 +108,6 @@ def main() -> None:
             if "currency_code" in transaction
             else transaction["operationAmount"]["currency"]["name"]
         )
-
         print(f"{date} {description}\n{from_transaction} -> {to_transaction}\nСумма: {amount} {currency_code}\n\n")
 
 
